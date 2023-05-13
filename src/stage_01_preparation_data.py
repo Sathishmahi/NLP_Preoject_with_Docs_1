87525156d3e04b3b77ddd1b4930d84289065fc36
Sathishmahi <sathishmahi433@gamil.com>
2023-05-13 15:51:10 +0530

    data preparation done

--- src/stage_01_preparation_data.py
+++ src/stage_01_preparation_data.py
@@ -1,9 +1,12 @@
 import argparse
 import os
 import shutil
+from urllib.request import urlretrieve
+from pathlib import Path
 from tqdm import tqdm
 import logging
 from src.utils.common import read_yaml, create_directories
+from src.utils.data_management import process_posts
 import random
 
 
@@ -17,11 +20,39 @@ logging.basicConfig(
     )
 
 
+def download_data(url:str,file_path:Path):
+    create_directories(path_to_directories=[os.path.split(file_path)[0]])
+    urlretrieve(url=url,filename=file_path)
 def main(config_path, params_path):
-    ## read config files
+
     config = read_yaml(config_path)
     params = read_yaml(params_path)
-    pass
+
+    source_data_content=config.get("source_data")
+    data_url=source_data_content.get("data_url")
+    data_dir_name=source_data_content.get("data_dir")
+    data_file_name=source_data_content.get("data_file")
+    file_path=os.path.join(data_dir_name,data_file_name)
+    download_data(url=data_url,file_path=file_path)
+
+    prepare_data_content=params.get('prepare_data')
+    split=prepare_data_content.get("split") 
+    seed=prepare_data_content.get("seed") 
+    random.seed(seed)
+
+    artifacts_content=config.get("artifacts")
+    preapred_data_dir=os.path.join(artifacts_content.get("ARTIFACTS_DIR")
+                                    ,artifacts_content.get("PREPARED_DATA"))
+    create_directories(path_to_directories=[Path(preapred_data_dir)])
+
+    train_data_path=os.path.join(preapred_data_dir,artifacts_content.get('TRAIN_DATA'))
+    test_data_path=os.path.join(preapred_data_dir,artifacts_content.get('TEST_DATA'))
+
+    encode="utf8"
+    with open(file=file_path,encoding=encode) as fd_in:
+        with open(file=train_data_path,encoding=encode,mode="w") as fd_out_train:
+            with open(file=test_data_path,encoding=encode,mode="w") as fd_out_test:
+                process_posts(fd_in,fd_out_train,fd_out_test,"<python>",split)
 
 
 if __name__ == '__main__':
---
+++ src/utils/data_management.py
@@ -0,0 +1,24 @@
+from tqdm import tqdm
+import random
+import xml.etree.ElementTree as et
+import re
+# from src.stage_01_preparation_data import logging
+
+def process_posts(fd_in,fd_out_train,fd_out_test,target_tag,split):
+    line_no=1
+    for line_info in tqdm(fd_in):
+        try:
+            fd_write= fd_out_train if random.random()>split else fd_out_test
+            attr=et.fromstring(line_info).attrib
+            tid=attr.get("Id","")
+            label= 1 if target_tag in attr.get("Tags","") else 0
+            title=re.sub("/s+", "", attr.get("Title","")).strip()
+            body=re.sub("/s+", "", attr.get("Body","")).strip()
+            text=f"{title} {body}"
+            fd_write.write(f"{tid}\t{text}\t{label}\n")
+            line_no+=1
+        except Exception as e:
+            mag=f"skip line no {line_no}  :  {e}\n"
+            # logging.exception(msg=msg)
+
+
