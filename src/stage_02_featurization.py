f11025cd5f9afc1bac28b39b07af1a6c82cbc5f7
Sathishmahi <sathishmahi433@gamil.com>
2023-05-14 10:48:38 +0530

    stage 02 featurization done

--- src/stage_02_featurization.py
+++ src/stage_02_featurization.py
@@ -3,8 +3,10 @@ import os
 import shutil
 from tqdm import tqdm
 import logging
-from src.utils.common import read_yaml, create_directories
+from src.utils.common import read_yaml, create_directories,get_df,to_save_pkl
 import random
+from pathlib import Path
+from sklearn.feature_extraction.text import CountVectorizer,TfidfTransformer
 
 
 STAGE = "STAGE FEATURIZATION" ## <<< change stage name 
@@ -21,8 +23,51 @@ def main(config_path, params_path):
     ## read config files
     config = read_yaml(config_path)
     params = read_yaml(params_path)
-    pass
+    
+    artifacts_content=config.get("artifacts")
+    preapred_data_dir=os.path.join(artifacts_content.get("ARTIFACTS_DIR")
+                                    ,artifacts_content.get("PREPARED_DATA"))
 
+    train_data_path=os.path.join(preapred_data_dir,artifacts_content.get('TRAIN_DATA'))
+    test_data_path=os.path.join(preapred_data_dir,artifacts_content.get('TEST_DATA'))
+
+    featurize_content=params.get("featurize")
+    no_of_features=featurize_content.get("no_of_features")
+    n_grams=featurize_content.get("n_grams")
+    column_names=featurize_content.get("column_names")
+
+    feature_dir_path=os.path.join(artifacts_content.get("ARTIFACTS_DIR"),artifacts_content.get("FEATURIZED_DATA"))
+    feature_train_data_path=os.path.join(feature_dir_path,artifacts_content.get("FEATURIZED_DATA_TRAIN"))
+    feature_test_data_path=os.path.join(feature_dir_path,artifacts_content.get("FEATURIZED_DATA_TEST"))
+
+    create_directories(path_to_directories=[feature_dir_path])
+
+    df_train=get_df(file_path=train_data_path, column_names=column_names)
+    df_test=get_df(file_path=test_data_path, column_names=column_names)
+
+    train_text=df_train.text.str.lower().values.astype('U')
+    bag_of_words=CountVectorizer(
+        stop_words="english",
+        max_features=no_of_features,
+        ngram_range=(1,n_grams)
+        )
+
+    bag_of_words.fit(train_text)
+    train_binary_matrix=bag_of_words.transform(train_text)
+
+    tfidf=TfidfTransformer(smooth_idf=False)
+    tfidf.fit(train_binary_matrix)
+    tfidf_train_matrix=tfidf.transform(train_binary_matrix)
+
+
+    test_text=df_test.text.str.lower().values.astype('U')
+    test_binary_matrix=bag_of_words.transform(test_text)
+    tfidf_test_matrix=tfidf.transform(test_binary_matrix)
+
+    train_pkl=os.path.join(feature_dir_path,artifacts_content.get("FEATURIZED_DATA_TRAIN"))
+    test_pkl=os.path.join(feature_dir_path,artifacts_content.get("FEATURIZED_DATA_TEST"))
+
+    to_save_pkl(file_paths=[Path(train_pkl),Path(test_pkl)], objs=[tfidf_train_matrix,tfidf_test_matrix])
 
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
--- src/utils/common.py
+++ src/utils/common.py
@@ -3,8 +3,16 @@ import yaml
 import logging
 import time
 import pandas as pd
+import pickle
 import json
+from pathlib import Path
 
+
+def get_df(file_path:Path,column_names:list,sep:str="\t",
+            header=None,encoding="utf8")->pd.DataFrame:
+    df = pd.read_csv(file_path,sep=sep,header=header,encoding=encoding,names=column_names)
+    logging.info(f'input dataframe path {file_path}  data frame size is {df.shape}')
+    return df
 def read_yaml(path_to_yaml: str) -> dict:
     with open(path_to_yaml) as yaml_file:
         content = yaml.safe_load(yaml_file)
@@ -16,6 +24,12 @@ def create_directories(path_to_directories: list) -> None:
         os.makedirs(path, exist_ok=True)
         logging.info(f"created directory at: {path}")
 
+def to_save_pkl(file_paths:list,objs):
+    if len(file_paths)!=len(objs):
+        raise Exception("file paths and objs list must be a same len")
+    for file_path,obj in zip(file_paths,objs): 
+        with open(file_path,'wb') as pkl_file:
+            pickle.dump(obj=obj, file=pkl_file)
 
 def save_json(path: str, data: dict) -> None:
     with open(path, "w") as f:
